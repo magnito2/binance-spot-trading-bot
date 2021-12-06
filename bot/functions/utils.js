315ba5e6ab8c81f6498ce47373f741e6e00f2df2
Magnus Otwani <magnusotwani@gmail.com>
2021-12-06 17:25:56 +0300

    store candlestick data in an object, add function to fetch the object through websockets and through REST

--- bot/functions/utils.js
+++ bot/functions/utils.js
@@ -3,7 +3,8 @@ const winston = require('winston');
 const fs = require('fs');
 const cron = require('node-cron');
 
-import { avgPrice30, getAllOrders } from './info'
+import { avgPrice30, avgPrice, getAllOrders } from './info';
+import { kRepo } from '../server';
 
 const logDir = 'logs';
 if (!fs.existsSync(logDir)) {
@@ -74,12 +75,12 @@ export const sendErrors = (message, st) => {
 export const getRSI = async function (st) {
     let upMoves = 0
     let downMoves = 0
-    const averagePrice = await avgPrice30(`${st.MAIN_MARKET}`, st)
-    averagePrice.forEach((element, index) => {
-        if (element[1] < element[4]) {
+
+    kRepo.sticks.forEach((kline, index) => {
+        if (kline.open < kline.close) {
             upMoves += 1
         }
-        if (element[1] > element[4]) {
+        if (kline.open > kline.close) {
             downMoves += 1
         }
     });
@@ -129,3 +130,114 @@ export const profitTracker = async (io, obj) => {
     });
 }
 
+//Make a klines object that will store set number klines
+
+export function Kline(open, high, low, close, vol, openTime, closeTime){
+    this.open = Number(open);
+    this.high = Number(high);
+    this.low = Number(low);
+    this.close = Number(close);
+    this.vol = Number(vol);
+    this.openTime = openTime;
+    this.closeTime = closeTime;
+}
+
+export function CandlesRepo (size, klines = []) {
+    this.size = size;
+    this.sticks = klines;
+}
+
+CandlesRepo.prototype.isEmpty = function () {
+    return this.sticks.length == 0;
+}
+
+CandlesRepo.prototype.add = function (kline) {
+    if(this.sticks.length < this.size){
+        this.sticks.push(kline);
+        this.sticks.sort((a,b) => {
+            if(a.openTime < b.openTime) return -1;
+            if(a.openTime > b.openTime) return 1;
+            return 0;
+        });
+    }
+    else if(this.last().openTime < kline.openTime){
+        this.sticks.push(kline);
+        this.sticks.shift();
+    } else {
+        console.log(`Cannot add an older kline than the latest, latest ${this.last().openTime}, adding ${kline.openTime}`)
+    }
+}
+
+CandlesRepo.prototype.peek = function (index = 0) {
+    return !this.isEmpty() && this.sticks.length > index ? this.sticks[index]  : undefined;
+}
+
+CandlesRepo.prototype.last = function () {
+    return !this.isEmpty() ? this.sticks[this.sticks.length - 1]  : undefined;
+}
+
+CandlesRepo.prototype.length = function () {
+    return this.sticks.length;
+}
+
+CandlesRepo.prototype[Symbol.iterator] = function() {
+    let index = 0;
+    return {
+        next: () => {
+            return index < this.sticks.length ? {
+                value: this.sticks[index++],
+                done: false
+            } : {
+                done: true
+            }
+        }
+    }
+}
+
+export const addKlineFromWS = (repo) => (obj) => {
+    //only add the final closing kline for the timeframe
+    if(obj.k.x){
+        const kline = new Kline(
+            obj.k.o, //open price
+            obj.k.h, //high price
+            obj.k.l, //low price
+            obj.k.c, //close price
+            obj.k.v, //base volume
+            obj.k.t, //start time
+            obj.k.T //close time
+        );
+        repo.add(kline);
+    
+        console.log(`kline has been added, close ${kline.close}, new length ${repo.length()}`);
+    }
+}
+
+export const addKlinesFromREST = (repo) => async (st) => {
+
+    const resp = await avgPrice30(`${st.MAIN_MARKET}`, st)
+
+    if(resp.hasOwnProperty('msg')){
+        console.error(resp.msg);
+        return;
+    }
+
+    const arr = resp;
+
+    if(!Array.isArray(arr)){
+        console.error(`${arr} expected an array`);
+        return
+    }
+
+    arr.forEach((kline) =>{
+        const candle = new Kline(
+            kline[1], //open price
+            kline[2], //high price
+            kline[3], //low price
+            kline[4], //close price
+            kline[5], //base volume
+            kline[0], //start time
+            kline[6] //close time
+        );
+        repo.add(candle);
+    });
+}
